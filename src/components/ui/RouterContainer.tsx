80153f39927a873c94bee0c34eb6a61aac6348fc
Michael Oberst <mioberst@gmx.de>
2023-02-13 18:49:40 +0100

    add dynamic import again respecting Rollup limitations

--- src/components/ui/RouterContainer.tsx
+++ src/components/ui/RouterContainer.tsx
@@ -1,13 +1,13 @@
-// import { lazy, ReactElement, Suspense } from "react"
+import { lazy, ReactElement, Suspense } from "react"
 import { Routes, Route } from "react-router-dom"
-// import { SectionData } from "../../types"
+import { SectionData } from "../../types"
 import { sectionsData } from "../../consts/sections-data"
 import Account from "../../views/Account"
 import Welcome from "../../views/Welcome"
-// import Loading from "./Loading"
+import Loading from "./Loading"
 import Login from "../auth/Login"
 import Signup from "../auth/Signup"
-import Intro_1 from "../introductions/Intro-1"
+// import Intro_1 from "../introductions/Intro-1"
 import Section_1_1 from "../sections/Section-1-1"
 
 // create routes of sections for lazy imports
@@ -33,18 +33,20 @@ import Section_1_1 from "../sections/Section-1-1"
 //     })
 // })
 
+const INTRO_1 = lazy(() => import("../sections/Section-1-1.jsx"))
+
 export default function RouterContainer() {
     return (
-        // <Suspense fallback={<Loading />}>
+        <Suspense fallback={<Loading />}>
             <Routes>
                 <Route path="/" element={<Welcome />} />
                 <Route path="/account" element={<Account />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
-                <Route path="/section-1" element={<Intro_1 sectionData={sectionsData[0]} />} />
-                <Route path="/section-1-1" element={<Section_1_1 sectionData={sectionsData[0].subSections![0]} />} />
+                <Route path="/section-1" element={<INTRO_1 sectionData={sectionsData[0]} />} />
+                {/* <Route path="/section-1-1" element={<Section_1_1 sectionData={sectionsData[0]} />} /> */}
                 {/* {sectionRoutes} */}
             </Routes>
-        // </Suspense>
+        </Suspense>
     )
 }
