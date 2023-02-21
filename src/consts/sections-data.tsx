705b3f078100d5e503ebcf09233cbf75687276d3
Michael Oberst <mioberst@gmx.de>
2023-02-22 00:58:21 +0100

    add missing page numbers to metadata

--- src/consts/sections-data.tsx
+++ src/consts/sections-data.tsx
@@ -9,7 +9,7 @@ export const sectionsData: SectionData[] = [
             {
                 title: "Einleitung",
                 lessonId: "1-1",
-                pageNumbers: "A1/B1-A16/B29"
+                pageNumbers: "A1/B1–A16/B29"
             }
         ]
     },
@@ -19,21 +19,20 @@ export const sectionsData: SectionData[] = [
         description: "Die Transzendentale Ästhetik befasst sich mit der Sinnlichkeit. Sie legt dar, dass Raum und Zeit die Formen der Anschauung sind und diese nur die Formen der Erscheinungen, nicht aber der Dinge an sich sind.",
         subSections: [
             {
-                title: "part 2-1",
+                title: "Transzendentale Ästhetik, § 1",
                 lessonId: "2-1",
+                pageNumbers: "A19/B33–A22/B36"
             },
             {
-                title: "part 2-2",
+                title: "Transzendentale Ästhetik, §§ 2–3",
                 lessonId: "2-2",
+                pageNumbers: "A22/B37–A30/B45"
             },
             {
-                title: "part 2-3",
+                title: "Transzendentale Ästhetik, §§ 4–8",
                 lessonId: "2-3",
-            },
-            {
-                title: "part 2-4",
-                lessonId: "2-4",
-            },
+                pageNumbers: "A30/B46–A49/B66, B66–69, B71–73"
+            }
         ]
     },
 ]
